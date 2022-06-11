3fd939a63ad3836433a87f0316c3c5e902871f36
Raziz1 <rahimaziz31@gmail.com>
2022-06-10 20:39:42 -0400

    Skills

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ for (let i = 0; i < button.length; i++) {
         }
     })
 }
+
+if (/Mobi/.test(navigator.userAgent)) {
+    // mobile!
+} else{
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("triggerAnimation", entry.isIntersecting);
+    });
+  };
+  
+  const Obs = new IntersectionObserver(inViewport);
+  const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+  
+  // Attach observer to every [data-inviewport] element:
+  const ELs_inViewport = document.querySelectorAll('.skills-columns');
+  ELs_inViewport.forEach(EL => {
+    Obs.observe(EL, obsOptions);
+  });
+}
