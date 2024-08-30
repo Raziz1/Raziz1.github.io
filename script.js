a721b077c15596aedc7c73e8e22d32a0d65aa2e5
Rahim Aziz <rahimaziz31@gmail.com>
2024-08-30 18:53:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -40,8 +40,8 @@ if (/Mobi/.test(navigator.userAgent)) {
 
 var typed = new Typed('.typewriter-title', {
   strings: ['⚡Electrical Engineering @ <b>McMaster University</b>',
-  '🔋Hardware Development Intern @ <b>Ford</b>',
   '🔋High Voltage Lead @ <b>MAC Formula Electric</b>',
+  '🔋Prev Hardware Development Intern @ <b>Ford</b>',
   ],
   typeSpeed: 35,
   loop: true,
