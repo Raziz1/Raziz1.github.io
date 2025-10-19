6337ce03bb3d744f8159be0b8904cf286b55b6f1
Rahim Aziz <rahimaziz31@gmail.com>
2025-10-19 14:28:57 -0700

    Updated skills

--- src/helpers/SkillsList.js
+++ src/helpers/SkillsList.js
@@ -6,19 +6,20 @@ import JavaIcon from '../assets/skills-icon/java-128.webp';
 import JavascriptIcon from '../assets/skills-icon/javascript-128.webp';
 import MatlabIcon from '../assets/skills-icon/matlab-128.webp';
 
-import VSIcon from '../assets/skills-icon/vs-128.webp';
-import VSCodeIcon from '../assets/skills-icon/vs-code-128.webp';
-import AndroidStudioIcon from '../assets/skills-icon/android-studio-128.webp';
-import EclipseIcon from '../assets/skills-icon/java-eclipse-128.webp';
 import UnityIcon from '../assets/skills-icon/unity-128.webp';
 
-import GitIcon from '../assets/skills-icon/git-128.webp';
 import ArduinoIcon from '../assets/skills-icon/arduino-128.webp';
 import RaspberryPiIcon from '../assets/skills-icon/raspberry-pi-128.webp';
 import STM32Icon from '../assets/skills-icon/stm32.webp';
 import AltiumIcon from '../assets/skills-icon/altium.webp';
-import InventorIcon from '../assets/skills-icon/inventor-128.webp';
-import AbletonIcon from '../assets/skills-icon/ableton-128.webp';
+import KiCadIcon from '../assets/skills-icon/ki-cad.webp';
+import PLECSIcon from '../assets/skills-icon/plecs.webp';
+import LTSpiceIcon from '../assets/skills-icon/ltspice.webp';
+import FusionIcon from '../assets/skills-icon/fusion.webp';
+import PSPiceIcon from '../assets/skills-icon/pspice.webp';
+import InventorIcon from '../assets/skills-icon/inventor.webp';
+import AllegroIcon from '../assets/skills-icon/allegro.webp';
+
 
 const skills = {
     Languages: [
@@ -30,21 +31,22 @@ const skills = {
       { src: JavascriptIcon, alt: 'Javascript' },
       { src: MatlabIcon, alt: 'MATLAB' },
     ],
-    IDE: [
-      { src: VSIcon, alt: 'Visual Studio' },
-      { src: VSCodeIcon, alt: 'Visual Studio Code' },
-      { src: AndroidStudioIcon, alt: 'Android Studio' },
-      { src: EclipseIcon, alt: 'Eclipse' },
-      { src: UnityIcon, alt: 'Unity' },
-    ],
-    Other: [
-      { src: GitIcon, alt: 'Git' },
+    Tools: [
       { src: ArduinoIcon, alt: 'Arduino' },
       { src: RaspberryPiIcon, alt: 'Raspberry Pi' },
-      { src: STM32Icon, alt: 'STM32 Cube IDE Designer Icon' },
-      { src: AltiumIcon, alt: 'Altium Designer Icon' },
+      { src: STM32Icon, alt: 'STM32' },
+      
+      { src: AltiumIcon, alt: 'Altium Designer' },
+      { src: KiCadIcon, alt: 'KiCad' },
+      { src: AllegroIcon, alt: 'Allegro' },
+
+      { src: LTSpiceIcon, alt: 'LTSpice' },
+      { src: PLECSIcon, alt: 'PLECS' },
+      { src: PSPiceIcon, alt: 'PSPice' },
+
+      { src: FusionIcon, alt: 'Fusion 360' },
       { src: InventorIcon, alt: 'Inventor Autodesk' },
-      { src: AbletonIcon, alt: 'Ableton' },
+      { src: UnityIcon, alt: 'Unity' },
     ]
   };
   
